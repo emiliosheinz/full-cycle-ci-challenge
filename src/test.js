df4ccc6711d590680a610234493628840902d3f5
Emilio Schaedler Heinzmann <emiliosheinz@gmail.com>
2023-11-04 20:34:58 -0300

    refactor: :recycle: add sum function tests

---
+++ src/test.js
@@ -0,0 +1,17 @@
+const { sum, sub, mult, div } = require('./')
+
+describe('Testing arithmetic functions', () => {
+  describe('sum function', () => {
+    test('adds positive numbers', () => {
+      expect(sum(1, 2)).toBe(3)
+    })
+
+    test('adds negative numbers', () => {
+      expect(sum(-1, -2)).toBe(-3)
+    })
+
+    test('adds positive and negative numbers', () => {
+      expect(sum(-1, 2)).toBe(1)
+    })
+  })
+})
